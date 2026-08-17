e96e1901144a9c798923fa5cd7ac72105c6292c7
tjb-official <tbat89@gmail.com>
2026-08-17 18:42:33 +0100

    2026-07-18 Transfer

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE = 'xsmini-v1';
+const ASSETS = [
+  '/xsmini/',
+  '/xsmini/index.html',
+  '/xsmini/manifest.json',
+  '/xsmini/icon-192x192.png',
+  '/xsmini/icon-512x512.png'
+];
+
+// Install: cache all assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: remove old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve from cache, fall back to network
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
